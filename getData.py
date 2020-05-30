9b422e37545e35d80dde5a2690067e9d75865d7c
devshah2 <35283231+devshah2@users.noreply.github.com>
2020-05-31 02:14:46 +0530

    Update getData.py

--- getData.py
+++ getData.py
@@ -1,16 +1,27 @@
-# import nltk
+import sys
 import util
 import requests
 from bs4 import BeautifulSoup
 import re
+import argparse
 
-link="https://icpe2020.spec.org/program-committee/"
-page = requests.get(link)
-soup = BeautifulSoup(page.content, 'html.parser')
-soup.beautify
-soup=soup.get_text()
-data=list(set(re.split(r'\n|\t| {2}|:|,', soup)))
-
+parser = argparse.ArgumentParser(description='Find data about researchers')
+parser.add_argument('-l','--link', action="store", help="Enter link to scrape", dest="link")
+parser.add_argument('-n','--names', action="store", help="Enter list of names seperated by commas", dest="names")
+args = parser.parse_args()
+#test link="https://icpe2020.spec.org/program-committee/"
+if(args.link!=None):
+    link=args.link
+    page = requests.get(link)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    soup.beautify
+    soup=soup.get_text()
+    data=list(set(re.split(r'\n|\t| {2}|:|,', soup)))
+elif(args.names!=None):
+    data=args.names.split(",")
+else:
+    print("Enter some value try -h for help")
+    sys.exit()
 util.run(data)
 ddd=util.data
 citedby=[x[0] for x in ddd]
